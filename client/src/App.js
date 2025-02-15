18b9178a7150777f6cce79495496491f96f676a9
Ishasnath Gayan <ishanath.gayan@ancsis.com>
2025-02-15 16:42:58 +0530

    nevigation left panel added

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,33 @@ import MainExplorer from "./MainExplorer";
 
 function App(props) {
 	const [displayContent, setDisplayContent] = useState({flag: "main",	epoch: -1, });
-	
-	  const switchFunction = (props) => {
-		console.log(`HELLO SWITCH: ${props.epoch}`);
-		setDisplayContent(props);
-	  };
+	const [navIndex, setNavIndex] = useState(0);
+
+	const toggleNavIndex = (index) => {
+		console.log(`HELLO TOGGLE: ${index}`);
+		setNavIndex(index);
+	};	
+	const switchFunction = (props) => {
+	console.log(`HELLO SWITCH: ${props.epoch}`);
+	setDisplayContent(props);
+	};
 
 	return(
-		<div>
-			{displayContent.flag === "main" ? (
-				<MainExplorer callbackSwitch={switchFunction} dataProvider={props.dataProvider}/>
-			) : (
-				<EntryExplorer	callbackSwitch={switchFunction}	epoch={displayContent.epoch}/>
-			)}
+		<div id="appBackPanel">
+			<div id="leftPanel">
+				<div className = {"menuBack" + (navIndex===0 ? " selected":"")} onClick={()=>toggleNavIndex(0)}> Explore</div>
+				<div className = {"menuBack" + (navIndex===1 ? " selected":"")} onClick={()=>toggleNavIndex(1)}> Reports</div>
+			</div>
+			<div id="rightPanel">
+				{
+					navIndex === 0 ?
+					(
+						displayContent.flag === "main" ? (
+							<MainExplorer callbackSwitch={switchFunction} dataProvider={props.dataProvider}/>) : 
+							(<EntryExplorer	callbackSwitch={switchFunction}	epoch={displayContent.epoch}/>)
+					) : <></>
+				}
+			</div>
 		</div>
 	)
 }
